9e5b8eee2bcd15747eec9f7541b321899884165f
istvanfoldy <istvan.foldy@optimonk.com>
2022-08-14 14:22:50 +0200

    use constructor pattern

--- module.js
+++ module.js
@@ -1,70 +1,78 @@
-const UI = (function () {
-  const prodImgSelector = ".js-product-img";
-  const prodNameSelector = ".js-product-name";
-  const prodCatSelector = ".js-product-category";
-  const prodDescSelector = ".js-product-description";
-  const prodPriceSelector = ".js-product-price";
+const UI = function () {
+  this.prodImgSelector = ".js-product-img";
+  this.prodNameSelector = ".js-product-name";
+  this.prodCatSelector = ".js-product-category";
+  this.prodDescSelector = ".js-product-description";
+  this.prodPriceSelector = ".js-product-price";
 
-  const prodImgEl = document.querySelector(prodImgSelector);
-  const prodNameEl = document.querySelector(prodNameSelector);
-  const prodCatEl = document.querySelector(prodCatSelector);
-  const prodDescEl = document.querySelector(prodDescSelector);
-  const prodPriceEl = document.querySelector(prodPriceSelector);
+  this.prodImgEl = document.querySelector(this.prodImgSelector);
+  this.prodNameEl = document.querySelector(this.prodNameSelector);
+  this.prodCatEl = document.querySelector(this.prodCatSelector);
+  this.prodDescEl = document.querySelector(this.prodDescSelector);
+  this.prodPriceEl = document.querySelector(this.prodPriceSelector);
+};
 
-  return {
-    setProdImg: (src) => {
-      prodImgEl.src = src;
-    },
-    setProdName: (name) => {
-      prodNameEl.textContent = name;
-    },
-    setProdCat: (category) => {
-      prodCatEl.textContent = category;
-    },
-    setProdDesc: (description) => {
-      prodDescEl.textContent = description;
-    },
-    setProdPrice: (price) => {
-      prodPriceEl.textContent = price;
-    },
-  };
-})();
+UI.prototype.setProdImg = function (src) {
+  this.prodImgEl.src = src;
+};
 
-const Product = (function () {
-  const name = "DisCatcher Target";
-  const category = "Discgolf";
-  const description =
+UI.prototype.setProdName = function (name) {
+  this.prodNameEl.textContent = name;
+};
+
+UI.prototype.setProdCat = function (category) {
+  this.prodCatEl.textContent = category;
+};
+
+UI.prototype.setProdDesc = function (description) {
+  this.prodDescEl.textContent = description;
+};
+
+UI.prototype.setProdPrice = function (price) {
+  this.prodPriceEl.textContent = price;
+};
+
+const Product = function () {
+  this.imageSrc = "src/img/discatcher.jpeg";
+  this.name = "DisCatcher Target";
+  this.category = "Discgolf";
+  this.description =
     "a chain grid that catches fast and slow putts, heavy and light discs like no other target";
-  const imageSrc = "src/img/discatcher.jpeg";
-  const price = 399;
+  this.price = 399;
+};
 
-  return {
-    getName: () => {
-      return name;
-    },
-    getCategory: () => {
-      return category;
-    },
-    getDesc: () => {
-      return `${name} is ${description}`;
-    },
-    getImg: () => {
-      return imageSrc;
-    },
-    getPrice: () => {
-      return `€${price}`;
-    },
-  };
-})();
+Product.prototype.getImg = function () {
+  return this.imageSrc;
+};
 
-const productImg = Product.getImg();
-const productName = Product.getName();
-const productCat = Product.getCategory();
-const productDesc = Product.getDesc();
-const productPrice = Product.getPrice();
+Product.prototype.getName = function () {
+  return this.name;
+};
 
-UI.setProdImg(productImg);
-UI.setProdName(productName);
-UI.setProdCat(productCat);
-UI.setProdDesc(productDesc);
-UI.setProdPrice(productPrice);
+Product.prototype.getCategory = function () {
+  return this.category;
+};
+
+Product.prototype.getDesc = function () {
+  return `${this.name} is ${this.description}`;
+};
+
+Product.prototype.getPrice = function () {
+  return `€${this.price}`;
+};
+
+const ProductFactory = new Product();
+
+const productImg = ProductFactory.getImg();
+const productName = ProductFactory.getName();
+const productCat = ProductFactory.getCategory();
+const productDesc = ProductFactory.getDesc();
+const productPrice = ProductFactory.getPrice();
+
+const ShopUI = new UI();
+
+ShopUI.setProdImg(productImg);
+ShopUI.setProdName(productName);
+ShopUI.setProdCat(productCat);
+ShopUI.setProdDesc(productDesc);
+ShopUI.setProdPrice(productPrice);
